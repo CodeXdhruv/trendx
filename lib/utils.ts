a92928448a8c2db584ce01d51e05f8184ca5b5ca
codexdhruv <dhruvsen24@gmail.com>
2026-06-27 17:44:22 +0530

    chore(frontend): update utility functions and static media assets

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getTickerIconUrl(ticker: string) {
+  // Strip any exchange prefixes/suffixes if necessary, or just use the raw ticker
+  const cleanTicker = ticker.split(/[:.]/)[0].toUpperCase();
+  return `https://img.logo.dev/ticker/${cleanTicker}?token=pk_U0ryq2cpRQKlxRGCer9tSw`;
+}
